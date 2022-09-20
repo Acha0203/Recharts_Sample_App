d9e71e8d13e338ecbb9d19b1154ea09914ea6b07
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2022-09-20 19:19:30 +0900

    feat: create the basic study chart component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import './App.css';
 import StudyChart from './components/StudyChart';
 
-function App() {
-  return (
-  <div className='App'>
+const App = () => (
+  <div className="App">
     <StudyChart />
-  </div>);
-}
+  </div>
+);
 
 export default App;
--- src/components/StudyChart.tsx
+++ src/components/StudyChart.tsx
@@ -10,31 +10,29 @@ import {
 } from 'recharts';
 import studyDataList from './studyData';
 
-const StudyChart = () => {
-  return (
-    <div className="container">
-      <LineChart
-        width={700}
-        height={300}
-        data={studyDataList}
-        margin={{
-          top: 5,
-          right: 20,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
-        <YAxis dataKey="問題数" />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="問題数" stroke="#8884d8" />
-        <Line type="monotone" dataKey="正解数" stroke="#3ba2f6" />
-        <Line type="monotone" dataKey="正解率" stroke="#ff0092" />
-      </LineChart>
-    </div>
-  );
-};
+const StudyChart = () => (
+  <div className="container">
+    <LineChart
+      width={700}
+      height={300}
+      data={studyDataList}
+      margin={{
+        top: 5,
+        right: 5,
+        left: 5,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="date" />
+      <YAxis dataKey="問題数" />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="問題数" stroke="#8884d8" />
+      <Line type="monotone" dataKey="正解数" stroke="#3ba2f6" />
+      <Line type="monotone" dataKey="正解率" stroke="#ff0092" />
+    </LineChart>
+  </div>
+);
 
 export default StudyChart;
